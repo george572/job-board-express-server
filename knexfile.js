285f9d016d8376931a6a226e281cfa1cf8691441
george572 <g.khutiashvili@gmail.com>
2025-03-12 01:33:03 +0400

    sss

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: '127.0.0.1',
-      user: 'postgres',
-      password: '1234',
-      database: 'samushao',
+      host: "127.0.0.1",
+      user: "postgres",
+      password: "1234",
+      database: "samushao",
     },
   },
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL + "?sslmode=require",
-    migrations: { tableName: 'knex_migrations' },
+    migrations: { tableName: "knex_migrations" },
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 };
