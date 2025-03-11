0d8da13a68fa4ffc69ccd243399b82b911165b45
george572 <g.khutiashvili@gmail.com>
2025-03-11 23:54:27 +0400

    s

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,5 @@
 require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
 
-// Try to read certificate file if it exists
-let ca;
-try {
-  ca = fs.readFileSync(path.join(__dirname, 'global-bundle.pem')).toString();
-} catch (e) {
-  ca = undefined;
-}
 module.exports = {
   development: {
     client: 'pg',
@@ -21,7 +12,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + "?sslmode=require",
+    connection: process.env.DATABASE_URL + "?sslmode=disable",
     ssl: {
       rejectUnauthorized: true,
     },
