291cc1c22632892af7d164927f6a72c910d8cce0
george572 <g.khutiashvili@gmail.com>
2025-03-12 02:08:15 +0400

    s

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const cloudinary = require("cloudinary").v2;
 const app = express();
 const port = process.env.PORT || 3000;
 
-const knex = require("knex");
-const db = knex(require("./knexfile").development); 
-
 app.use(cors()); // Allow all origins
 app.use(express.json());
 
@@ -42,40 +39,6 @@ const sendCvRouter = require("./routes/sendCv");
 app.use("/send-cv", sendCvRouter);
 
 app.use("/uploads", express.static("uploads"));
-
-const categories = [
-  "გაყიდვები",
-  "მენეჯმენტი",
-  "ინფორმაციული ტექნოლოგიები",
-  "ტურიზმი",
-  "სამედიცინო",
-  "საბანკო-საფინანსო",
-  "საწყობი",
-  "დისტრიბუცია",
-  "აზარტული",
-  "უსაფრთხოება",
-  "მზარეული",
-  "მოლარე",
-  "დრაივები",
-  "მარკეტინგი",
-  "ბუღალტერია",
-  "ლოჯისტიკა",
-  "ადმინისტრაცია",
-  "კურიერი",
-  "ფინანსები",
-  "ავტოინდუსტრია",
-  "მშენებლობა",
-  "ინჟინერია",
-  "ადამიანური რესურსები",
-  "ფარმაცია",
-  "პროექტების მენეჯმენტი",
-  "სხვა",
-];
-
-db('categories')
-  .insert(categories.map(name => ({ name })))
-  .then(() => console.log('Categories inserted successfully'))
-  .catch((err) => console.error('Error inserting categories:', err));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
