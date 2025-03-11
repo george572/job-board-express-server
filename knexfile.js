1b09c7da1f545035a0151853b0a8cf14ca20e77d
george572 <g.khutiashvili@gmail.com>
2025-03-11 23:59:58 +0400

    sss

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + "?sslmode=require",
-    pool: { min: 2, max: 10 },
     migrations: { tableName: 'knex_migrations' },
   },
 };
