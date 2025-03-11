39bd73b57e195754ec8eabffe0b1df621e69f65d
george572 <g.khutiashvili@gmail.com>
2025-03-11 22:34:56 +0400

    ssl

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + "?sslmode=require",
     pool: { min: 2, max: 10 },
     ssl: {
       rejectUnauthorized: false
