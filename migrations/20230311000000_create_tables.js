dce94556f9169ff40ddd85d615d0a09c01727e22
george572 <g.khutiashvili@gmail.com>
2025-03-12 14:18:33 +0400

    migration

--- migrations/20230311000000_create_tables.js
+++ migrations/20230311000000_create_tables.js
@@ -56,20 +56,3 @@ exports.down = function (knex) {
     .dropTableIfExists("jobs")
     .dropTableIfExists("categories");
 };
-
-exports.up = function (knex) {
-  return knex.schema.alterTable("company_logos", (table) => {
-    table
-      .integer("job_id")
-      .unsigned()
-      .references("id")
-      .inTable("jobs")
-      .onDelete("CASCADE"); // Delete logo when job is deleted
-  });
-};
-
-exports.down = function (knex) {
-  return knex.schema.alterTable("company_logos", (table) => {
-    table.dropForeign("job_id");
-  });
-};
--- migrations/20250312101759_alter_company_logos_add_job_id_foreign_key.js
+++ migrations/20250312101759_alter_company_logos_add_job_id_foreign_key.js
@@ -1,13 +1,12 @@
+
 exports.up = function (knex) {
     return knex.schema.alterTable("company_logos", (table) => {
-      table.dropForeign("job_id"); // Remove existing foreign key (if any)
       table
         .integer("job_id")
         .unsigned()
-        .notNullable()
         .references("id")
         .inTable("jobs")
-        .onDelete("CASCADE"); // Ensure cascade deletion
+        .onDelete("CASCADE"); // Delete logo when job is deleted
     });
   };
   
