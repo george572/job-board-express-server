ffef950a90e86d6861f682b9bee4bec6053d85e9
george572 <g.khutiashvili@gmail.com>
2026-02-07 18:47:20 +0400

    baba

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors()); // Allow all origins
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 cloudinary.config({
   cloud_name: "dd7gz0aqv",
