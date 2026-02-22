395777fbcbba5a8e8a68fd601b1ec065e81a5d2b
george572 <g.khutiashvili@gmail.com>
2026-02-22 21:28:50 +0400

    knexfile update

--- knexfile.js
+++ knexfile.js
@@ -1,48 +1,21 @@
-require("dotenv").config();
-
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      host: "ep-frosty-mode-aglu6csx-pooler.c-2.eu-central-1.aws.neon.tech",
-      user: "neondb_owner",
-      password: "npg_dtCoyp2jBh7H",
-      database: "neondb",
-      ssl: { rejectUnauthorized: false }
-    },
-    pool: {
-      min: 2,
-      max: 10,
-      acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 10000,
-      createTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      afterCreate: (conn, done) => {
-        console.log("[knex] New DB connection created");
-        done(null, conn);
-      },
-    },
-  },
   production: {
     client: "pg",
     connection: {
-      host: "ep-frosty-mode-aglu6csx-pooler.c-2.eu-central-1.aws.neon.tech",
+      host: "ep-frosty-mode-aglu6csx.c-2.eu-central-1.aws.neon.tech",  // Direct host, NO pooler
       user: "neondb_owner",
       password: "npg_dtCoyp2jBh7H",
       database: "neondb",
       ssl: { rejectUnauthorized: false }
     },
-    pool: {
-      min: 2,
-      max: 10,
-      acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 10000,
-      createTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      afterCreate: (conn, done) => {
-        console.log("[knex] New DB connection created");
-        done(null, conn);
-      },
+    pool: {  // Keep Knex pool for direct connections
+      min: 0,    // Let it scale to zero
+      max: 8,    // Conservative for Fly
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 5000
+      // NO afterCreate - kills performance
     },
-  },
+    // Add query logging only if debugging
+    debug: false
+  }
 };
