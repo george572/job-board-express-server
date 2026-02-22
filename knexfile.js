14f77c4cff51f2a87831886d148d7d1f7e9007c1
george572 <g.khutiashvili@gmail.com>
2026-02-22 17:50:35 +0400

    s

--- knexfile.js
+++ knexfile.js
@@ -8,8 +8,20 @@ module.exports = {
       user: "neondb_owner",
       password: "npg_dtCoyp2jBh7H",
       database: "neondb",
-      ssl: { rejectUnauthorized: false }  // Now properly included
-    }
+      ssl: { rejectUnauthorized: false }
+    },
+    pool: {
+      min: 2,
+      max: 10,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 10000,
+      createTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      afterCreate: (conn, done) => {
+        console.log("[knex] New DB connection created");
+        done(null, conn);
+      },
+    },
   },
   production: {
     client: "pg",
@@ -18,7 +30,19 @@ module.exports = {
       user: "neondb_owner",
       password: "npg_dtCoyp2jBh7H",
       database: "neondb",
-      ssl: { rejectUnauthorized: false }  // Now properly included
-    }
-  }
+      ssl: { rejectUnauthorized: false }
+    },
+    pool: {
+      min: 2,
+      max: 10,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 10000,
+      createTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      afterCreate: (conn, done) => {
+        console.log("[knex] New DB connection created");
+        done(null, conn);
+      },
+    },
+  },
 };
